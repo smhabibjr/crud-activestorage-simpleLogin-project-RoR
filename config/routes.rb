0da56606f20dfdd4c9ee543b9c241ca5fea1619d
smhabibjr <smhabib.abir2030@gmail.com>
2022-01-24 14:44:36 +0100

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   #post 'update_records' => 'home#update'
   #get 'home' => 'home#index'
-  resources :home, only: [:index]
+  root 'home#index'
   post 'update' => 'home#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
