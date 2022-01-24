3544926c72827bdfb7ffd1b96e3044534a6a5e53
smhabibjr <smhabib.abir2030@gmail.com>
2022-01-24 14:50:46 +0100

    deleted all controller

--- app/assets/javascripts/product.js
+++
@@ -1,8 +0,0 @@
-$(".product.index").ready(function (){
-    console.log("Hi this is product show page");
-})
-
-
-$(".product.show").ready(function (){
-    console.log("Hi this is product show page");
-})
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
 
-
-  #post 'update_records' => 'home#update'
-  #get 'home' => 'home#index'
-  root 'home#index'
-  post 'update' => 'home#update'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
