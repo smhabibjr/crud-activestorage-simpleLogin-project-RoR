df050cc484daf25bb29ea732007fd44a1c26e97d
smhabibjr <smhabib.abir2030@gmail.com>
2022-01-25 14:20:23 +0100

    crud is dode

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,26 +12,41 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(article_params)
+    article = Article.new(article_params)
 
-    if @article.save
-      redirect_to @article
+    if article.save
+      redirect_to article
     else
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @article_edit = Article.find(params[:id])
+  end
+
+  def update
+    article_update = Article.find(params[:id])
+
+    if article_update.update(article_params)
+      redirect_to article_update
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+
 
   private
   def article_params
     params.require(:article).permit(:title, :body)
   end
 
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 end
