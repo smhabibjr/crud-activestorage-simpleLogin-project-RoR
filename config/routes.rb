baafa188393e45dcff982a916d564f44907964c8
smhabibjr <smhabib.abir2030@gmail.com>
2022-01-25 14:29:13 +0100

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
 
+  get 'student/index'
+  get 'student/show'
+  get 'student/new'
+  get 'student/create'
+  get 'student/edit'
+  get 'student/update'
+  get 'student/destroy'
   root 'articles#index'
   resources :articles
 end
