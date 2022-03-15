062938d3deadbc3897e6e63501602392eb66ea53
HabibJR <smhabib.abir2030@gmail.com>
2022-03-15 08:53:42 +0100

    [T4]add jquery and bootstrap

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
   def index
+    @all_articles = Article.all
+    @all_students = Student.all
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+import 'stylesheets/application'
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "@fortawesome/fontawesome-free/css/all"
 require('jquery')
-
-import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/css/bootstrap'
-import 'stylesheets/application'
 
 Rails.start()
 Turbolinks.start()
